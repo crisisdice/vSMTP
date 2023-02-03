ac878403e64d7c3d6d1ca5e0ea7f872649231d91
Noé Breton <noe.breton@epitech.eu>
2023-02-03 12:57:07 +0100

    feat(Vqueue): Home page

--- src/vqueue/src/cli/debugger/ui.rs
+++ src/vqueue/src/cli/debugger/ui.rs
@@ -17,16 +17,32 @@
 use crate::cli::args::Commands;
 use tui::{
     backend::CrosstermBackend,
-    widgets::{Block, Borders},
-    //layout::{Layout, Constraint, Direction},
+    widgets::{Block, Borders, BorderType, Tabs, Paragraph},
+    layout::{Layout, Constraint, Direction, Alignment},
+    text::{Span, Spans},
+    style::{Color, Modifier, Style},
     Terminal,
-    //Frame,
 };
 use crossterm::{
     event::{read, Event, KeyCode, KeyEvent, KeyModifiers, KeyEventKind, KeyEventState},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
+#[derive(Copy, Clone, Debug)]
+enum MenuItem {
+    Home,
+    Vqueue,
+}
+
+impl From<MenuItem> for usize {
+    fn from(input: MenuItem) -> usize {
+        match input {
+            MenuItem::Home => 0,
+            MenuItem::Vqueue => 1,
+        }
+    }
+}
+
 #[allow(clippy::multiple_inherent_impl)]
 impl Commands {
     /// setup crossterm terminal 
@@ -35,22 +51,69 @@ impl Commands {
     /// # Panics
     /// 
     /// TODO
-    /// 
+    /// Ajouter x vqueue en zone de texte et les faire clickable pour afficher leurs mails
     #[inline] pub fn ui() -> Result<(), std::io::Error> {
         // crate terminal
         enable_raw_mode()?;
+        let menu_titles = vec!["Home", "Vqueue","Escape"];
+        let mut active_menu_item = MenuItem::Home;
+        
         let mut stdout = std::io::stdout();
         execute!(stdout, EnterAlternateScreen)?;
         let backend = CrosstermBackend::new(stdout);
         let mut terminal = Terminal::new(backend)?;
         
         terminal.draw(|f| {
-            let size = f.size();
-            let block = Block::default()
-                .title("Running vqueue (/var/spool/vsmtp)")
-                .borders(Borders::ALL);
-            f.render_widget(block, size);
-            
+        let size = f.size();
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(2)
+            .constraints(
+            [
+                Constraint::Length(3),
+                Constraint::Min(2),
+                Constraint::Length(3),
+            ]
+            .as_ref(),
+            )
+            .split(size);
+        let menu = menu_titles
+            .iter()
+            .map(|t| {
+            let (first, rest) = t.split_at(1);
+            Spans::from(vec![
+                Span::styled(
+                    first,
+                    Style::default()
+                        .fg(Color::Yellow)
+                        .add_modifier(Modifier::UNDERLINED),
+                ),
+                Span::styled(rest, Style::default().fg(Color::White)),
+            ])
+            })
+            .collect();
+
+        let tabs = Tabs::new(menu)
+            .select(active_menu_item.into())
+            .block(Block::default().title("Menu").borders(Borders::ALL))
+            .style(Style::default().fg(Color::White))
+            .divider(Span::raw("|"));
+
+        f.render_widget(tabs, chunks[0]);
+
+        match active_menu_item {
+            MenuItem::Home => {
+                f.render_widget(Self::home_page(), size)
+            }
+            MenuItem::Vqueue => {
+                let queue_chunks = Layout::default()
+                        .direction(Direction::Horizontal)
+                        .constraints(
+                            [Constraint::Percentage(20), Constraint::Percentage(80)].as_ref(),
+                        )
+                        .split(chunks[1]);
+            }
+        }
         })?;
         // replace by event key escape
         match read().unwrap() {
@@ -69,6 +132,14 @@ impl Commands {
                 terminal.show_cursor()?;
             },
             Event::Key(KeyEvent{
+                code: KeyCode::Char('v'),
+                modifiers: KeyModifiers::NONE,
+                kind: KeyEventKind::Press,
+                state: KeyEventState::NONE
+            }) => {
+                active_menu_item = MenuItem::Vqueue;
+            },
+            Event::Key(KeyEvent{
                 code: KeyCode::Up,
                 modifiers: KeyModifiers::NONE,
                 kind: KeyEventKind::Press,
@@ -84,8 +155,41 @@ impl Commands {
             }) => {
                 // ici faire la selection entre les différentes QUEUE
             },
+            Event::Key(KeyEvent{
+                code: KeyCode::Enter,
+                modifiers: KeyModifiers::NONE,
+                kind: KeyEventKind::Press,
+                state: KeyEventState::NONE
+            }) => {
+                // ici entré dans une VQUEUE et voir les différents mail qui la composent
+            }
             _ =>{}
         };
     Ok(())
+    }
+
+    pub fn home_page() -> Paragraph<'static>{
+        let home = Paragraph::new(vec![
+            Spans::from(vec![Span::raw("")]),
+            Spans::from(vec![Span::raw("Welcome")]),
+            Spans::from(vec![Span::raw("")]),
+            Spans::from(vec![Span::raw("to")]),
+            Spans::from(vec![Span::raw("")]),
+            Spans::from(vec![Span::styled(
+                "UI command",
+                Style::default().fg(Color::LightBlue),
+            )]),
+            Spans::from(vec![Span::raw("")]),
+            Spans::from(vec![Span::raw("Press 'V' to access Vqueue, 'Esc' to quit this terminal.")]),
+        ])
+        .alignment(Alignment::Center)
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .style(Style::default().fg(Color::White))
+                .title("Home")
+                .border_type(BorderType::Plain),
+        );
+        home
     }
 }
