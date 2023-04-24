436ee04b286c5bb6888f03353e9c8c2f1c26a520
Ilian Baylon <ilian.baylon@epitech.eu>
2023-04-23 19:50:43 -0700

    feat(dnsbl): check and lookup functions for spamhaus and spamrats

--- src/plugins/vsmtp-plugin-dnsbl/src/api.rs
+++ src/plugins/vsmtp-plugin-dnsbl/src/api.rs
@@ -16,27 +16,83 @@
 */
 
 use rhai::plugin::*;
+use vsmtp_common::{status::Status, Reply};
+use trust_dns_resolver::Resolver;
+use trust_dns_resolver::config::*;
+
+enum BlockListKinds {
+    Spamhaus,
+    Spamrats,
+    Default
+}
+
+fn blocklist_values(bl: &String) -> BlockListKinds {
+    match bl.as_str() {
+        "spamhaus" => BlockListKinds::Spamhaus,
+        "spamrats" => BlockListKinds::Spamrats,
+        _ => BlockListKinds::Default
+    }
+}
+
+fn blocklist_urls(bl: BlockListKinds) -> Option<String> {
+    match bl {
+        BlockListKinds::Spamhaus => Some(String::from("zen.spamhaus.org")),
+        BlockListKinds::Spamrats => Some(String::from("all.spamrats.com")),
+        BlockListKinds::Default => None
+    }
+}
 
 #[derive(Debug, serde::Deserialize)]
 struct DnsblParameters {
-    pub kind: String
+    pub bl: Vec<String>
 }
 
 pub struct Dnsbl {
-    pub kind: String
+    pub bl: Vec<String>
 }
 
 impl Dnsbl {
     pub fn check(&self, domain: String) -> Result<bool, Box<rhai::EvalAltResult>> {
-        Ok(true)
+        let resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
+        let mut result = false;
+        for element in &self.bl {
+            let bl_kind = blocklist_values(element);
+            let response = resolver.lookup_ip(domain.clone() + "." + blocklist_urls(bl_kind).unwrap().as_str());
+            match response {
+                Ok(res) => {
+                    dbg!(res);
+                    result = true;
+                    break;
+                },
+                Err(err) => {
+                    dbg!(err);
+                    result = false;
+                }
+            }
+        }
+        Ok(result)
     }
 
-    pub fn choose_db() {
-
-    }
-
-    pub fn translate_results() {
-
+    pub fn lookup(&self, domain: String) -> Result<Status, Box<rhai::EvalAltResult>> {
+        let resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
+        for element in &self.bl {
+            let bl_kind = blocklist_values(element);
+            let response = resolver.lookup_ip(domain.clone() + "." + blocklist_urls(bl_kind).unwrap().as_str());
+            match response {
+                Ok(res) => {
+                    dbg!(res);
+                    return Ok(Status::Deny(
+                        "XXX spam detected\r\n"
+                            .parse::<Reply>()
+                            .unwrap(),
+                    ));
+                },
+                Err(err) => {
+                    dbg!(err);
+                }
+            }
+        }
+        Ok(Status::Next)
     }
 }
 
@@ -47,10 +103,14 @@ pub mod vsmtp_plugin_dnsbl {
     #[rhai_fn(global, return_raw)]
     pub fn build(parameters: rhai::Map) -> Result<Bl, Box<rhai::EvalAltResult>> {
         let parameters = rhai::serde::from_dynamic::<DnsblParameters>(&parameters.into())?;
+        let mut bl_list = Vec::<String>::new();
 
-        println!("Building DNSBL with kind: {}", parameters.kind);
+        println!("Building DNSBL with bl:");
+        for element in parameters.bl {
+            bl_list.push(dbg!(element));
+        }
         Ok(rhai::Shared::new(Dnsbl {
-            kind: parameters.kind
+            bl: bl_list
         }))
     }
 
@@ -60,5 +120,13 @@ pub mod vsmtp_plugin_dnsbl {
         domain: rhai::Dynamic
     ) -> Result<bool, Box<rhai::EvalAltResult>> {
         con.check(domain.to_string())
+    }
+
+    #[rhai_fn(global, return_raw, pure)]
+    pub fn lookup(
+        con: &mut Bl,
+        domain: rhai::Dynamic
+    ) -> Result<Status, Box<rhai::EvalAltResult>> {
+        con.lookup(domain.to_string())
     }
 }
