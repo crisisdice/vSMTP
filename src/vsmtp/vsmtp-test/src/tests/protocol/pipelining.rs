42d4ed4138f57aeba8b11bc035cf230b5de9bf25
LangArthur <arthur.lang.pro@gmail.com>
2023-04-27 14:15:00 +0200

    Test for pipelining

--- src/vsmtp/vsmtp-test/src/tests/protocol/pipelining.rs
+++ src/vsmtp/vsmtp-test/src/tests/protocol/pipelining.rs
@@ -15,6 +15,7 @@
  *
 */
 
+use crate::config;
 use crate::run_test;
 
 run_test! {
@@ -59,3 +60,195 @@ run_test! {
         "221 Service closing transmission channel\r\n",
     ],
 }
+
+run_test! {
+    fn sneaky_unrecognized_command, // TODO: check what to do in this case
+    input = [
+        "EHLO foobar\r\n",
+        "MAIL FROM:<john@doe>\r\n\
+        RCPT TO:<fitz@trusted>\r\n\
+        NOTACOMMAND and its args\
+        DATA\r\n",
+        "QUIT\r\n",
+    ],
+    expected = [
+        "220 testserver.com Service ready\r\n",
+        "250-testserver.com\r\n",
+        "250-STARTTLS\r\n",
+        "250-8BITMIME\r\n",
+        "250-SMTPUTF8\r\n",
+        "250 PIPELINING\r\n",
+        "250 Ok\r\n\
+        250 Ok\r\n\
+        503 Bad sequence of commands\r\n",
+        "221 Service closing transmission channel\r\n",
+
+    ],
+}
+
+run_test! {
+    fn no_rcpt,
+    input = [
+        "EHLO foobar\r\n",
+        "MAIL FROM:<john@doe>\r\n\
+        DATA\r\n",
+        "QUIT\r\n",
+    ],
+    expected = [
+        "220 testserver.com Service ready\r\n",
+        "250-testserver.com\r\n",
+        "250-STARTTLS\r\n",
+        "250-8BITMIME\r\n",
+        "250-SMTPUTF8\r\n",
+        "250 PIPELINING\r\n",
+        "250 Ok\r\n\
+        503 Bad sequence of commands\r\n",
+        "221 Service closing transmission channel\r\n",
+
+    ],
+}
+
+run_test! {
+    fn wrong_rcpt,
+    input = [
+        "EHLO foobar\r\n",
+        "MAIL FROM:<john@doe>\r\n\
+        RCPT TO:<galvin@>\r\n\
+        DATA\r\n",
+        "QUIT\r\n",
+    ],
+    expected = [
+        "220 testserver.com Service ready\r\n",
+        "250-testserver.com\r\n",
+        "250-STARTTLS\r\n",
+        "250-8BITMIME\r\n",
+        "250-SMTPUTF8\r\n",
+        "250 PIPELINING\r\n",
+        "250 Ok\r\n\
+        554 permanent problems with the remote server\r\n",
+        "221 Service closing transmission channel\r\n",
+    ],
+}
+
+run_test! {
+    fn multiple_rcpt,
+    input = [
+        "EHLO foobar\r\n",
+        "MAIL FROM:<john@doe>\r\n\
+        RCPT TO:<henry@trusted.com>\r\n\
+        RCPT TO:<galvin@trusted.com>\r\n\
+        DATA\r\n",
+        &("X".repeat(10) + "\r\n.\r\n"),
+        "QUIT\r\n",
+    ],
+    expected = [
+        "220 testserver.com Service ready\r\n",
+        "250-testserver.com\r\n",
+        "250-STARTTLS\r\n",
+        "250-8BITMIME\r\n",
+        "250-SMTPUTF8\r\n",
+        "250 PIPELINING\r\n",
+        "250 Ok\r\n\
+        250 Ok\r\n\
+        554 permanent problems with the remote server\r\n\
+        354 Start mail input; end with <CRLF>.<CRLF>\r\n",
+        "250 Ok\r\n",
+        "221 Service closing transmission channel\r\n",
+    ]
+}
+
+run_test! {
+    fn multiple_rcpt_with_some_wrong,
+    input = [
+        "EHLO foobar\r\n",
+        "MAIL FROM:<john@doe>\r\n\
+        RCPT TO:<henry@trusted.com>\r\n\
+        RCPT TO:<galvin@malicious.com>\r\n\
+        DATA\r\n",
+        &("X".repeat(10) + "\r\n.\r\n"),
+        "QUIT\r\n",
+    ],
+    expected = [
+        "220 testserver.com Service ready\r\n",
+        "250-testserver.com\r\n",
+        "250-STARTTLS\r\n",
+        "250-8BITMIME\r\n",
+        "250-SMTPUTF8\r\n",
+        "250 PIPELINING\r\n",
+        "250 Ok\r\n\
+        250 Ok\r\n\
+        554 permanent problems with the remote server\r\n\
+        354 Start mail input; end with <CRLF>.<CRLF>\r\n",
+        "250 Ok\r\n",
+        "221 Service closing transmission channel\r\n",
+    ]
+}
+
+run_test! {
+    fn reset_after_data,
+    input = [
+        "EHLO foobar\r\n",
+        "MAIL FROM:<john@doe>\r\n\
+        RCPT TO:<henry@trusted.com>\r\n\
+        RCPT TO:<galvin@malicious.com>\r\n\
+        DATA\r\n",
+        &("X".repeat(10) + "\r\n.\r\nRESET\r\n"),
+        "MAIL FROM:<john@doe>\r\n",
+        "QUIT\r\n",
+    ],
+    expected = [
+        "220 testserver.com Service ready\r\n",
+        "250-testserver.com\r\n",
+        "250-STARTTLS\r\n",
+        "250-8BITMIME\r\n",
+        "250-SMTPUTF8\r\n",
+        "250 PIPELINING\r\n",
+        "250 Ok\r\n\
+        250 Ok\r\n\
+        554 permanent problems with the remote server\r\n\
+        354 Start mail input; end with <CRLF>.<CRLF>\r\n",
+        "250 Ok\r\n",
+        "250 Ok\r\n",
+        "221 Service closing transmission channel\r\n",
+    ]
+}
+
+// FIXME: this test
+run_test! {
+    fn error_after_data,
+    input = [
+        "EHLO foobar\r\n",
+        "MAIL FROM:<john@doe>\r\n\
+        RCPT TO:<henry@trusted.com>\r\n\
+        RCPT TO:<galvin@malicious.com>\r\n\
+        DATA\r\n",
+        &("X".repeat(10) + "\r\n.\r\nnotacommand\r\n"),
+        "MAIL FROM:<john@doe>\r\n",
+        "QUIT\r\n",
+    ],
+    expected = [
+        "220 testserver.com Service ready\r\n",
+        "250-testserver.com\r\n",
+        "250-STARTTLS\r\n",
+        "250-8BITMIME\r\n",
+        "250-SMTPUTF8\r\n",
+        "250 PIPELINING\r\n",
+        "250 Ok\r\n\
+        250 Ok\r\n\
+        554 permanent problems with the remote server\r\n\
+        354 Start mail input; end with <CRLF>.<CRLF>\r\n",
+        "250 Ok\r\n",
+        "250 Ok\r\n",
+        "221 Service closing transmission channel\r\n",
+    ]
+}
+
+/* Command statuses MUST be coordinated with responses by counting each
+separate response and correlating that count with the number of
+commands known to have been issued. Multiline responses MUST be
+supported. Matching on the basis of either the error code value or
+associated text is expressly forbidden. */
+
+// MAIL FROM:<john@doe>
+// RCPT TO:<galvin@n>
+// DATA
